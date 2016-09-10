54fa9b8d68d844065c9731dfdd9ac435b62c3caf
wen2720 <wenhao2720@gmail.com>
2016-09-10 16:41:55 +0200

    ex1.3 wen

--- week1/ex1.1/wen/findWord.sh
+++ week1/ex1.1/wen/findWord.sh
@@ -1,3 +1,3 @@
-tr -sc "[:alpha:]" "\n" < cnn_news.txt | sort > test.txt
+tr -cs "[:alpha:]" "\n" < cnn_news.txt | sort > test.txt
 tr "[:upper:]" "[:lower:]" < test.txt > low2up.txt
-uniq -c low2up.txt > uniqWord.txt | sort - nr <uniqWord.txt | head -10
+uniq -c low2up.txt > uniqWord.txt | sort - nr < uniqWord.txt | head -10
