d6a4bf6b157a72a63038e0a19d93f0869d726328
wen2720 <wenhao2720@gmail.com>
2016-09-14 16:56:50 +0200

    ex2.2

--- week2/ex2.1/lis2matri.py
+++ week2/ex2.1/lis2matri.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python
+##fo1 = open('R9FVAdXW.txt')
+##content1 = fo1.readlines() ##it is a list
+##matrix = ''.join(content1)
+##print matrix 
+##fo2 = open('list_matrix.txt', 'w')
+#fo2.write("%s" %matrix)
+#fo1.close()
+#fo2.close()
+
+import json
 fo1 = open('R9FVAdXW.txt')
-content1 = fo1.readlines() ##it is a list
-matrix = ''.join(content1)
-print matrix 
+content1 = fo1.read()
+s = json.dumps(content1)
+print json.loads(s)
+
 fo2 = open('list_matrix.txt', 'w')
-fo2.write("%s" %matrix)
+fo2.write("%s" %json.loads(s))
 fo1.close()
 fo2.close()
+
--- week2/ex2.2/ttable.py
+++ week2/ex2.2/ttable.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python
+import json
+print("Enter the natural of input: ")
+in_put = input()
+
+def list_tt (in_put):
+	tt = []
+	for bit_val in range(2**in_put, 0, -1):
+		pattern=[]
+		for scale_val in range(in_put, 0, -1):
+			if bit_val >2**(scale_val-1):
+				bit_val = bit_val-2**(scale_val-1)
+				pattern.append(1)
+				#print 1
+			elif bit_val <= 2**(scale_val-1):
+				#print 0
+				pattern.append(0)
+		tt.append(pattern)
+	return tt
+
+
+if in_put <= 0:
+	print("invalid number")
+else:
+	print list_tt(in_put)
+
