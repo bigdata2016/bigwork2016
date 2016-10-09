724a3e8ec8b54d6fd63e7c0d612f61fe296a6d1b
wen2720 <wenhao2720@gmail.com>
2016-10-09 20:19:36 +0200

    Recap

--- week3/ex3.1/wen/gaus_elimination.py
+++ week3/ex3.1/wen/gaus_elimination.py
@@ -2,14 +2,9 @@
 
 import numpy as np
 
-A = np.loadtxt('XAhwshXe.txt', delimiter=',', usecols=(0,1,2))
+A = np.loadtxt('XAhwshXe.txt', delimiter=',', usecols=(0,1,2)) ##load file, delimite the array by ',' use nth column
 b = np.loadtxt('XAhwshXe.txt', delimiter=',', usecols=(3,))
 
 result = np.linalg.solve(A,b)
 
 print result
-
-
-
-
-
--- week3/ex3.2/wen/plot_polynomial.py
+++ week3/ex3.2/wen/plot_polynomial.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
-
 import numpy as np
-matrix = np.loadtxt('ENyYffaq.txt', delimiter=' ')
-print matrix
+from matplotlib import pyplot as mp
+from scipy.optimize import fsolve
+#from numpy.polynomial import polynomial as p
+
+x = np.loadtxt('ENyYffaq.txt', delimiter=' ', usecols=(0,))			#load x coodinates as array
+y = np.loadtxt('ENyYffaq.txt', delimiter=' ', usecols=(1,))			#load y coodinates as array
+#print x
+c = np.polyfit(x, y, 3)												#optimize a 3rd degree polynomial's coefficient
+#print c[0]
+#pow3 = lambda x : c[0]*x**3 + c[1]*x**2 + c[2]*x + c[3]			#diy function
+f = np.poly1d(c)													#return the polynomial form by the coefficient
+print f		
+r = fsolve(f, -20)													#solve root, start from a value of x
+print r
+##plot config
+mp.figure('3rd degree ploynomial')
+mp.suptitle('3rd degree ploynomial', fontweight='bold')
+mp.xlabel('X')
+mp.ylabel('Y')																
+mp.plot(x,y,r,'ro')					#plot x on y, root, show red
+mp.annotate('0,-1.43463629', xy=(0,-1.43463629), xytext=(2.5,5000),arrowprops=dict(arrowstyle='->'))	# show annotation			
+mp.grid()
+mp.show()
